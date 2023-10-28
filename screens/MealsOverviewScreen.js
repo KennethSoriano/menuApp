ba414bc7708b6d87cf5abff3a47e5481ed83206f
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-10-27 20:45:50 -0700

    Adding images and styling

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -12,9 +12,20 @@ function MealsOverviewScreen({ route }) {
     });
 
     function renderMealItem(itemData) {
+        const item = itemData.item;
+        const mealItemProps = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            complexity: item.complexity,
+            duration: item.duration,
+            affordability: item.affordability
+        }
+
         return (
-            <MealItem title={itemData.item.title} />
-        )
+            <MealItem 
+               {...mealItemProps}
+            />
+        );
     }
 
     return (
