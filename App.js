f9be7a2dd8b886a32d435554724e9ac7ef9a8c4e
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-10-28 19:06:42 -0700

    nexting navigators

--- App.js
+++ App.js
@@ -3,12 +3,36 @@ import { Button, StyleSheet, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401'},
+        headerTintColor: 'white',
+        sceneContainerStyle: {backgroundColor: '#3f2f25'}
+      }}
+    >
+      <Drawer.Screen 
+        name="Categories"
+        component={CategoriesScreen}
+      />
+      <Drawer.Screen
+        name='Favorites' 
+        component={FavoritesScreen}
+      />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -23,10 +47,10 @@ export default function App() {
           }}
         >
           <Stack.Screen 
-            name="MealsCategories" 
-            component={CategoriesScreen} 
+            name="Drawer" 
+            component={DrawerNavigator} 
             options={{
-              title: 'All Categories',
+              headerShown: false
             }} 
           />
           <Stack.Screen 
@@ -36,6 +60,9 @@ export default function App() {
           <Stack.Screen 
             name="MealDetail"
             component={MealDetailScreen}
+            options={{
+              title: 'About the Meal'
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
