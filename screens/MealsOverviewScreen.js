b2e89f82e25f1fe89f7702680f0b7dd00db433ed
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-10-29 08:51:03 -0700

    Managing Favorite Meals with the Context API

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,8 +1,7 @@
-import { FlatList, StyleSheet, View } from "react-native";
 import { useLayoutEffect } from "react";
 
 import { MEALS, CATEGORIES } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
+import MealsList from "../components/MealsList/MealsList";
 
 function MealsOverviewScreen({ route, navigation }) {
     const catId = route.params.categoryId;
@@ -20,41 +19,11 @@ function MealsOverviewScreen({ route, navigation }) {
         });
     }, [catId, navigation]);
 
-
-    function renderMealItem(itemData) {
-        const item = itemData.item;
-        const mealItemProps = {
-            title: item.title,
-            imageUrl: item.imageUrl,
-            complexity: item.complexity,
-            duration: item.duration,
-            affordability: item.affordability,
-            id: item.id
-        }
-
-        return (
-            <MealItem 
-               {...mealItemProps}
-            />
-        );
-    }
-
     return (
-        <View>
-            <FlatList
-                data={displayedMeals} 
-                keyExtractor={(item) => item.id}
-                renderItem={renderMealItem}
-            />
-        </View>
-    );
+        <MealsList items={displayedMeals} />
+    )
+
 }
 
 export default MealsOverviewScreen;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    },
-})
