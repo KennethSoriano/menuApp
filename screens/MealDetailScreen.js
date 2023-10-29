67fd4231688e417f8928e070b196ece8f16bf6b4
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-10-28 18:29:46 -0700

    adding icon button to header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -5,6 +5,7 @@ import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
 
 function MealDetailScreen({route, navigation}) {
     const mealId = route.params.mealId;
@@ -19,9 +20,10 @@ function MealDetailScreen({route, navigation}) {
         navigation.setOptions({
             headerRight: () => {
                 return (
-                    <Button 
-                        title='' 
-                        onPress = {headerButtonPressHandler}
+                    <IconButton
+                        icon="star"
+                        color="white"
+                        onPress={headerButtonPressHandler} 
                     />
                 )
             }
